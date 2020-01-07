f5a45fe14a24887f3b180f8729dbcfe8a543d8c6
Nicole Julian <nicolevjulian@gmail.com>
2020-01-07 00:34:33 -0500

    what did I use importlib for??? WHY did I use importlib for???

--- character_infos/adrian/bio.py
+++
@@ -1,7 +0,0 @@
-BIO = """
-    An expert in ordered, dark, and cold magics, Adrian inflicts subtle
-    devastation via horrifying mental and physical afflictions. Maybe some
-    purple flames if you really need them. That's his favorite color, after all?
-    Well, are you done yet? If you don't want Adrian, at least let him go
-    home already.
-"""

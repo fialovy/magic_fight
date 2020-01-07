1666e67b09ef567e97a56f610f76681d64e0bcf2
Norm Julian <normvjulian@gmail.com>
2020-01-06 23:32:53 -0500

    Initial commit of some data structuring

---
+++ character_infos/adrian/taunts.py
@@ -0,0 +1,7 @@
+TAUNTS = [
+    "I'm UTTERLY terrified.",
+    "Atrocious.",
+    "Sloppy. You're quite the inconvenience.",
+    "While we're at it, your outfit today is a disaster.",
+    "Can we get this over with?"
+]

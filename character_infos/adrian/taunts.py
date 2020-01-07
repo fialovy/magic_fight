ad0540a1676b62dad30c01965435674ba2058288
Norm Julian <normvjulian@gmail.com>
2020-01-07 00:34:33 -0500

    what did I use importlib for??? WHY did I use importlib for???

--- character_infos/adrian/taunts.py
+++
@@ -1,7 +0,0 @@
-TAUNTS = [
-    "I'm UTTERLY terrified.",
-    "Atrocious.",
-    "Sloppy. You're quite the inconvenience.",
-    "While we're at it, your outfit today is a disaster.",
-    "Can we get this over with?"
-]

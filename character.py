0d7f3ba38037eb3d79dd16612121c893f30f7363
Nicole Julian <njulian@shoobx.com>
2020-01-11 12:43:12 -0500

    whups; fix taunts json

--- character.py
+++ character.py
@@ -6,3 +6,9 @@ class Character:
         self.magic_info = magic_info
         self.taunts = taunts
         self.life = 10
+
+    def possibly_taunt(self):
+        """Depending on their chance of doing so (some characters are nicer),
+        pick and say a random taunt.
+        """
+        import pdb; pdb.set_trace()  

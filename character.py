b06d0599921450b35237b8f87e76c89ae5b7c349
Nicole Julian <njulian@shoobx.com>
2020-01-11 12:20:55 -0500

    make smaller modules and stuff

---
+++ character.py
@@ -0,0 +1,8 @@
+class Character:
+
+    def __init__(self, name, bio, magic_info, taunts):
+        self.name = name
+        self.bio = bio
+        self.magic_info = magic_info
+        self.taunts = taunts
+        self.life = 10

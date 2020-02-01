6d66574b3589986600069871d1723dda87b9de66
Nicole Julian <nicolevjulian@gmail.com>
2020-01-31 23:39:09 -0500

    this one is for a man called Jeremy

--- game.py
+++ game.py
@@ -10,10 +10,10 @@ from collections import namedtuple
 from character import Character
 
 
-CHARACTERS_DIR = 'characters'
+CHARACTERS_DIR = "characters"
 
-SpellChoice = namedtuple('SpellChoice', ['dimension', 'hit'])
-SpecialChoice = namedtuple('SpecialChoice', ['description', 'effect'])
+SpellChoice = namedtuple("SpellChoice", ["dimension", "hit"])
+SpecialChoice = namedtuple("SpecialChoice", ["description", "effect"])
 
 
 class Game:
@@ -25,31 +25,28 @@ class Game:
         self.set_up_characters()
 
     def set_up_characters(self):
-        for name in os.listdir(f'{CHARACTERS_DIR}'):
-            namepath = f'{CHARACTERS_DIR}/{name}'
+        for name in os.listdir(f"{CHARACTERS_DIR}"):
+            namepath = f"{CHARACTERS_DIR}/{name}"
 
-            with open(f'{namepath}/bio.txt', 'r') as bio_fl, \
-                    open(f'{namepath}/magic.json', 'r') as magic_fl, \
-                    open(f'{namepath}/taunts.json', 'r') as taunts_fl:
-                bio=bio_fl.read().strip()
-                magic_info=json.load(magic_fl)
-                taunts=json.load(taunts_fl)
+            with open(f"{namepath}/bio.txt", "r") as bio_fl, open(
+                f"{namepath}/magic.json", "r"
+            ) as magic_fl, open(f"{namepath}/taunts.json", "r") as taunts_fl:
+                bio = bio_fl.read().strip()
+                magic_info = json.load(magic_fl)
+                taunts = json.load(taunts_fl)
 
             character = Character(
-                name=name.title(),
-                bio=bio,
-                magic_info=magic_info,
-                taunts=taunts
+                name=name.title(), bio=bio, magic_info=magic_info, taunts=taunts
             )
 
-            special_path = f'{namepath}/special.json'
+            special_path = f"{namepath}/special.json"
             # Boo hoo; not everyone has special abilities right now.
             if os.path.exists(special_path):
-                with open(special_path, 'r') as special_fl:
-                    special_abilities_info=json.load(special_fl)
+                with open(special_path, "r") as special_fl:
+                    special_abilities_info = json.load(special_fl)
                 character.special_abilities_info = special_abilities_info
 
-            self.opponents[name] = character 
+            self.opponents[name] = character
 
     def get_input_choice(self, prompt, choices, capitalize_choice=True):
         """Given a custom prompt and list of text choices, prompt user to
@@ -63,19 +60,19 @@ class Game:
         while choice is None:
             print(prompt)
             for idx, item in choices.items():
-                print(f'{idx}: {item.title() if capitalize_choice else item}\n')
+                print(f"{idx}: {item.title() if capitalize_choice else item}\n")
 
-            choice = input('>>> ')
+            choice = input(">>> ")
             try:
                 choice = int(choice.strip())
             except Exception:
-                print('Please choose a number in the given range.')
+                print("Please choose a number in the given range.")
                 choice = None
                 continue
 
             # ...but the enum made this 'interface' easy to validate.
             if choice not in choices:
-                print('Please choose a number in the given range.')
+                print("Please choose a number in the given range.")
                 choice = None
 
         # Name key of chosen character
@@ -91,18 +88,18 @@ class Game:
 
         while not confirmed:
             print(prompt)
-            print(f'Confirm choice? Type y or n.')
+            print(f"Confirm choice? Type y or n.")
 
-            confirm = input('>>> ')
+            confirm = input(">>> ")
             try:
                 confirm = confirm.strip().lower()
             except Exception:
                 print('Please type "y" or "n"')
                 continue
 
-            if confirm == 'y':
+            if confirm == "y":
                 return choice
-            elif confirm == 'n':
+            elif confirm == "n":
                 return deny_func()
             else:
                 print('Please type "y" or "n"')
@@ -111,14 +108,14 @@ class Game:
     def select_character(self):
 
         chosen = self.get_input_choice(
-            prompt='Press a key to choose a character:\n',
+            prompt="Press a key to choose a character:\n",
             choices=self.opponents,
-            capitalize_choice=True
+            capitalize_choice=True,
         )
         chosen = self.confirm_input_choice(
             choice=chosen,
-            prompt=f'{self.opponents[chosen].bio}\n',
-            deny_func=self.select_character
+            prompt=f"{self.opponents[chosen].bio}\n",
+            deny_func=self.select_character,
         )
 
         return chosen
@@ -130,37 +127,34 @@ class Game:
         """
         choices = {}
 
-        for dimension, info in self.player.magic_info['deals'].items():
+        for dimension, info in self.player.magic_info["deals"].items():
             # Not everyone can do every kind of magic, which means they
             # might be better at fewer things.
-            if not info['spells']:
+            if not info["spells"]:
                 continue
             choice_key = f'{random.choice(info["spells"])} ({dimension})'
-            choices[choice_key] = SpellChoice(
-                dimension=dimension, hit=info['amount'])
+            choices[choice_key] = SpellChoice(dimension=dimension, hit=info["amount"])
 
         for ability_name, info in self.player.special_abilities_info.items():
             choices[ability_name] = SpecialChoice(
-                description=info['description'], effect=info['effect'])
+                description=info["description"], effect=info["effect"]
+            )
 
         return choices
 
     def hit(self, whom, dimension, max_hit):
         """Hit opponent with up to the amount of given dimension's magic they take."""
-        hit = min(
-            whom.magic_info['takes'][dimension]['amount'],
-            max_hit
-        )
+        hit = min(whom.magic_info["takes"][dimension]["amount"], max_hit)
         whom.life -= hit
-        print(f'{whom.name} takes {hit} {dimension} damage!\n')  # TODO: changing words here
+        print(
+            f"{whom.name} takes {hit} {dimension} damage!\n"
+        )  # TODO: changing words here
         time.sleep(1)
 
     def player_turn(self):
         spell_infos = self._construct_player_spell_choices()
         spell = self.get_input_choice(
-            prompt='Choose your spell:\n',
-            choices=spell_infos,
-            capitalize_choice=False
+            prompt="Choose your spell:\n", choices=spell_infos, capitalize_choice=False
         )
         choice = spell_infos[spell]
 
@@ -173,7 +167,7 @@ class Game:
             self.confirm_input_choice(
                 choice=spell,
                 prompt=description,
-                deny_func=self.player_turn  # this seems wrong...
+                deny_func=self.player_turn,  # this seems wrong...
             )
             ability_func = getattr(special_abilities, choice.effect)
             ability_func(self.player)
@@ -181,18 +175,16 @@ class Game:
     def opponent_turn(self):
         self.opponent.possibly_taunt()
 
-        spell_info = self.opponent.magic_info['deals']
+        spell_info = self.opponent.magic_info["deals"]
         # Recall that not everyone can deal every kind, as a cost to being
         # super strong in some.
-        able_dimensions = [
-            dim for dim, info in spell_info.items() if info['spells']
-        ]
+        able_dimensions = [dim for dim, info in spell_info.items() if info["spells"]]
         dimension = random.choice(able_dimensions)
-        spell = random.choice(spell_info[dimension]['spells'])
+        spell = random.choice(spell_info[dimension]["spells"])
 
         print(f'{self.opponent.name} chooses: "{spell}"')
         time.sleep(1)
-        self.hit(self.player, dimension, max_hit=spell_info[dimension]['amount'])
+        self.hit(self.player, dimension, max_hit=spell_info[dimension]["amount"])
 
     def play(self):
         chosen = self.select_character()
@@ -201,9 +193,9 @@ class Game:
         del self.opponents[chosen]
 
         self.opponent = random.choice(list(self.opponents.values()))
-        print(f'\n{self.opponent.name} wants to duel!\n')
+        print(f"\n{self.opponent.name} wants to duel!\n")
         time.sleep(1)
-        print('Ready?\n')
+        print("Ready?\n")
         time.sleep(2)
 
         # well it is a start
@@ -214,12 +206,14 @@ class Game:
 
             self.player_turn()
             if self.opponent.life <= 0:
-                print(f'You have defeated {self.opponent.name}! Congratulations, Sorcerer.')
+                print(
+                    f"You have defeated {self.opponent.name}! Congratulations, Sorcerer."
+                )
                 time.sleep(2)
                 return
 
             self.opponent_turn()
             if self.player.life <= 0:
-                print(f'{self.opponent.name} has bested you. Game over.')
+                print(f"{self.opponent.name} has bested you. Game over.")
                 time.sleep(2)
                 return
--- special_abilities.py
+++ special_abilities.py
@@ -2,6 +2,8 @@
 
 # Honora
 def change_to_norm(nora):
-    print('OMG YOU GOT HERE')
-    import pdb; pdb.set_trace()  
+    print("OMG YOU GOT HERE")
+    import pdb
+
+    pdb.set_trace()
     raise NotImplementedError

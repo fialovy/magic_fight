9e530a2ac83149cd05df3b75e6e983f3c7bc1554
Norm Julian <normvjulian@gmail.com>
2020-02-01 00:09:20 -0500

    if that is what you want, black

--- special_abilities.py
+++ special_abilities.py
@@ -9,13 +9,16 @@ def change_to_norm(nora):
     from game import CHARACTERS_DIR
 
     namepath = f"{CHARACTERS_DIR}/nora/norm"
-    with open(f"{namepath}/magic.json", "r") as magic_fl, \
-         open(f"{namepath}/taunts.json", "r") as taunts_fl:
+    with open(f"{namepath}/magic.json", "r") as magic_fl, open(
+        f"{namepath}/taunts.json", "r"
+    ) as taunts_fl:
         magic_info = json.load(magic_fl)
         taunts = json.load(taunts_fl)
 
     norm = Character(
-        name="Norm", bio="It's a long story, to be honest.",
-        magic_info=magic_info, taunts=taunts
+        name="Norm",
+        bio="It's a long story, to be honest.",
+        magic_info=magic_info,
+        taunts=taunts,
     )
     return norm

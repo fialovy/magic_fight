42365fb7828024366b43ef03dc161c245e821f03
Norm Julian <normvjulian@gmail.com>
2022-08-11 16:22:15 -0400

    _set_attr_from_file

--- character.py
+++ character.py
@@ -13,40 +13,81 @@ class Character:
         self.namepath = special_namepath or f"{CHARACTERS_DIR}/{name.lower()}"
 
         self._set_bio()
+        self._set_ascii()
         self._set_magic_info()
         self._set_taunts()
         self._set_reactions()
         self._set_special_abilities()
 
+    def _set_attr_from_file(self, attr, filepath, strip=False, allow_empty=False, empty_val=None):
+        """
+        Read and set character data from a file, and set the character attribute
+        to empty_val if file does not exist.
+
+        Expected file types for now include txt and json. Maybe we can use actual
+        typing at some point.
+
+        If strip is true, well...do strip() to get rid of whitespace on the ends.
+        """
+        if os.path.exists(filepath):
+            with open(filepath, "r") as attr_fl:
+                if filepath.endswith('txt'):
+                    val = attr_fl.read()
+                    if strip:
+                        val = val.strip()
+                elif filepath.endswith('json'):
+                    val = json.load(attr_fl)
+                else:
+                    raise ValueError(f"Unsupported filetype at the moment: {filepath}")
+            setattr(self, attr, val)
+        elif allow_empty:
+            setattr(self, attr, empty_val)
+        else:
+            raise FileNotFoundError(f"Could not set {attr}! Expected file not found: {filepath}")
+
     def _set_bio(self):
-        with open(f"{self.namepath}/bio.txt", "r") as bio_fl:
-            self.bio = bio_fl.read().strip()
+        self._set_attr_from_file(
+            attr="bio",
+            filepath=f"{self.namepath}/bio.txt",
+            strip=True,
+        )
+
+    def _set_ascii(self):
+        self._set_attr_from_file(
+            attr="ascii",
+            filepath=f"{self.namepath}/ascii_art.txt",
+            strip=True,
+            allow_empty=True,
+            empty_val=""
+        )
 
     def _set_magic_info(self):
-        with open(f"{self.namepath}/magic.json", "r") as magic_fl:
-            self.magic_info = json.load(magic_fl)
+        self._set_attr_from_file(
+            attr="magic_info",
+            filepath=f"{self.namepath}/magic.json",
+        )
 
     def _set_taunts(self):
-        with open(f"{self.namepath}/taunts.json", "r") as taunts_fl:
-            self.taunts = json.load(taunts_fl)
+        self._set_attr_from_file(
+            attr="taunts",
+            filepath=f"{self.namepath}/taunts.json",
+        )
 
     def _set_reactions(self):
-        reactions_path = f"{self.namepath}/reactions.json"
-        if os.path.exists(reactions_path):
-            with open(reactions_path, "r") as reactions_fl:
-                self.reactions = json.load(reactions_fl)
-        else:
-            self.reactions = None
+        self._set_attr_from_file(
+            attr="reactions",
+            filepath=f"{self.namepath}/reactions.json",
+            allow_empty=True,
+            empty_val=None
+        )
 
     def _set_special_abilities(self, special_path=None):
-        self.special_abilities_info = {}
-
-        special_path = special_path or f"{self.namepath}/special.json"
-        # Boo hoo; not everyone has special abilities right now.
-        if os.path.exists(special_path):
-            with open(special_path, "r") as special_fl:
-                special_abilities_info = json.load(special_fl)
-            self.special_abilities_info = special_abilities_info
+        self._set_attr_from_file(
+            attr="special_abilities_info",
+            filepath=f"{self.namepath}/special.json",
+            allow_empty=True,
+            empty_val={}
+        )
 
     def possibly_taunt(self):
         """Depending on their percent chance of doing so (some characters
